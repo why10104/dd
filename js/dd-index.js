9ac327cf2ee7079e9854cb6ed81a5cc78ce67d67
wwh <wwh@qq.com>
2018-08-03 22:21:50 +0800

    2js

--- js/dd-index.js
+++ js/dd-index.js
@@ -37,28 +37,67 @@ $(function(){
 
     //bannwer
 
-    var mySwiper = new Swiper ('.swiper-container', {
-        direction: 'vertical',
+    var mySwiper1 = new Swiper ('.cont1 .swiper-container', {
+        direction: 'horizontal',
         loop: true,
+        autoplay:true,
+        effect : 'fade',
 
         // 如果需要分页器
         pagination: {
             el: '.swiper-pagination',
+            clickable:true,
+            renderBullet: function (index, className) {
+                return '<span class="' + className + '">' + (index + 1) + '</span>';
+            },
         },
 
         // 如果需要前进后退按钮
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
+            hideOnClick :true,
         },
 
-        // 如果需要滚动条
-        scrollbar: {
-            el: '.swiper-scrollbar',
+    });
+
+    var mySwiper2 = new Swiper ('.cont2 .swiper-container', {
+        direction: 'horizontal',
+        loop: true,
+        autoplay:true,
+        effect : 'fade',
+
+        // 如果需要前进后退按钮
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+            hideOnClick :true,
+        },
+
+    });
+
+    var mySwiper3 = new Swiper ('.b-r-t .swiper-container', {
+        direction: 'horizontal',
+        loop: true,
+        autoplay:true,
+        effect : 'fade',
+        speed: 2000,
+
+        // 如果分页符
+        pagination: {
+            el: '.btt1 .swiper-pagination',
+            clickable:true,
+        },
+        // 如果需要前进后退按钮
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+            hideOnClick :true,
         },
     });
 
-        
+
+
 });
 
 
